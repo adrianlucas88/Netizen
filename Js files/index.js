17b470e7e6c17786e280af3df5f76c25e7da1c72
adrianlucas88 <pasha2366@gmail.com>
2021-10-28 10:12:04 +0100

    c

--- Js files/index.js
+++ Js files/index.js
@@ -1,13 +1,3 @@
-let tictacBtn = document.getElementById("tictac");
-let quizBtn = document.getElementById("quiz");
-let projectContainer = document.getElementById("project_container");
-
 function menuFunction(x) {
   x.classList.toggle("change");
-}
-
-function closeProjects() {
-  projectContainer.style.display = "none";
-  tictactoeContainer.style.display = "none";
-  quizWindow.style.visibility = "hidden";
 }
